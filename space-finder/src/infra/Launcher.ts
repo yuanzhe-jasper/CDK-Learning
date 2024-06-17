293499533e8a4fddc7413fa8a30c3902047aa757
apple <apple@appledeMacBook-Air.local>
2024-06-17 13:40:40 -0700

    update stacks

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -5,8 +5,10 @@ import { ApiStack } from "./stacks/ApiStack";
 
 
 const app = new App();
-new DataStack(app, 'DataStack');
-const lambdaStack = new LambdaStack(app, 'LambdaStack');
+const dataStack = new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+    spacesTable: dataStack.spacesTable
+});
 new ApiStack(app, 'ApiStack', {
     helloLambdaIntegration: lambdaStack.helloLambdaIntegration
 })
