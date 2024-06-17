769e116c35255895a407a4b3e655feee60230650
apple <apple@appledeMacBook-Air.local>
2024-06-17 12:02:22 -0700

    integrate apigateway and lammbda functon

---
+++ space-finder/src/infra/Launcher.ts
@@ -0,0 +1,12 @@
+import { App } from "aws-cdk-lib";
+import { DataStack } from "./stacks/DataStack";
+import { LambdaStack } from "./stacks/LambdaStack";
+import { ApiStack } from "./stacks/ApiStack";
+
+
+const app = new App();
+new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
+new ApiStack(app, 'ApiStack', {
+    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
+})
---
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,22 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { Code, Function as LambdaFunction, Runtime} from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { join } from "path";
+
+export class LambdaStack extends Stack {
+
+    public readonly helloLambdaIntegration: LambdaIntegration
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'hello.main',
+            code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
+        })
+
+        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
+    }
+}
